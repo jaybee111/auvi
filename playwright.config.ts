cc4dede20bb762f6c07a43535464934ddd78ecab
Jonas Boettcher <jones.boettcher@gmail.com>
2022-09-28 12:41:40 +0200

    Add webserver to playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -103,6 +103,12 @@ const config: PlaywrightTestConfig = {
   //   command: 'npm run start',
   //   port: 3000,
   // },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000/',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
 };
 
 export default config;
