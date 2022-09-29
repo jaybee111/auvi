4ab89a1b6eab29cf2c38eef2986d634b2af76d06
Jonas Boettcher <jones.boettcher@gmail.com>
2022-09-29 09:28:18 +0200

    Renamed CloseResultListListener.ts to ResultListCloseListener.ts

--- src/events/ResultListCloseListener.ts
+++ src/events/ResultListCloseListener.ts
@@ -1,6 +1,6 @@
 import EventListenerBase from "./EventListenerBase";
 
-export class CloseResultListListener extends EventListenerBase {
+export class ResultListCloseListener extends EventListenerBase {
     register() {
         this.store.events.subscribe('closeResultList', () => {
             if(this.store.components.resultList) {
