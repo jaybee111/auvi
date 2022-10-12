bc3fd1eafd3057e755b61a7a280b6f397b49e45c
Jonas Boettcher <jones.boettcher@gmail.com>
2022-10-12 13:36:01 +0200

    Feature: Close list on document click only in tooltip mode

--- src/events/ResultListCloseListener.ts
+++ src/events/ResultListCloseListener.ts
@@ -3,8 +3,10 @@ import EventListenerBase from "./EventListenerBase";
 export class ResultListCloseListener extends EventListenerBase {
     register() {
         this.store.events.subscribe('closeResultList', () => {
-            this.store.components.resultList?.classList.remove('is-open');
-            this.store.components.wrapper?.classList.remove('is-open');
+            if(this.store.options.mode === 'tooltip') {
+                this.store.components.resultList?.classList.remove('is-open');
+                this.store.components.wrapper?.classList.remove('is-open');
+            }
         });
     }
 }
